da0ab6806f132e8864cb2d8820f277f36f584dfd
youngjae124 <jyj1579@naver.com>
2025-10-31 13:59:25 +0900

    퀴즈 4 타입변환 , 연산자

---
+++ src/be_study/quiz/Quiz04.java
@@ -0,0 +1,89 @@
+package be_study.quiz;
+
+public class Quiz04 {
+
+	public static void main(String[] args) {
+/*		
+	//	1. 아래 1), *  2) 에 맞게 풀이.
+	int x = 10;								int result = 2;
+	int y = 4;								double result = 2.5;
+//	___result = ___x/y;
+	System.out.println(result);
+
+	1) 결과가 2가 나오도록 작성하세요  
+	2) 결과가 2.5가 나오도록 작성하세요  
+
+  int result11 = (int)x/y;
+  System.out.println(result11);
+  
+  double result12 = (double)x/y;
+  System.out.println(result12);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//	2. 결과가 7.5가 나오도록 작성하세요
+	double a = 3.5;   				double reasult = a=(int)b;
+	double b = 4.7;
+// 	double result = _______________;  double result = 7.5;
+	System.out.println(result);
+
+
+//	3. 계산결과가 12가 나오도록 작성하세요
+	String a = "3.4";
+	String b = "4";				 int result = (Integer.parseInt(a)*b);
+//	______ result = ________;
+	System.out.println(result);
+
+	//----------------------------------------------------
+
+//	4. 계산결과가 "1013" 이 나오도록 코드를 작성하세요.
+	String a = "10";
+	int b = 3;
+	double c = 4.5; 			System.out.println(Integer.parseInt((b*c+"10"));
+	_____ result = _______; 
+	System.out.println(result);
+
+
+	5. 계산결과가 "243" 이 나오도록 코드를 작성하세요.
+	int a = 4;
+	double b = 3.4;
+	String c = "6.8";			String result = (a + "" + (int)b + c);
+	_____ result = _______;
+	System.out.println(result);
+
+	6. 계산 결과가 아래와 같이 나오도록 코드를 작성하세요.
+	int x = 111;
+	int y = 13;						
+	int result = _____;
+	System.out.println(result);
+
+	1) 출력결과 : 7    int result = x % y;
+	2) 출력결과 : 8	int result = x / y;
+		
+		
+	*/	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
